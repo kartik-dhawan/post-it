b3321b7362f831c5b91f0f39e937de6a7f08134d
kartik dhawan <kartik.dhawan@3pillarglobal.com>
2022-07-18 14:14:30 +0530

    b1.0: created server routes, connected mongoose and set the schema/models

--- server/index.js
+++ server/index.js
@@ -1,1 +1,29 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import postRoutes from "./Routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://shelby:shelby101@cluster0.fbb80.mongodb.net/post-it?retryWrites=true&w=majority";
+
+const PORT = 3001;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log("Listening on port: " + PORT));
+  })
+  .catch((err) => console.log(err));
+
+app.use("/posts", postRoutes);
